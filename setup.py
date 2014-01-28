87598247ae1d9c1dca4e323fd71538ea9057c1da
Tom Daff <7022897+tdaff@users.noreply.github.com>
2014-01-28 18:27:09 -0500

    enhancement: not much benefit from cython, mark here in case it comes back

--- setup.py
+++ setup.py
@@ -3,26 +3,11 @@
 from distutils.core import setup
 from glob import glob
 
-try:
-    import Cython.Build
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
-
-cmdclass = {}
-ext_modules = []
-
-if use_cython:
-    ext_modules = Cython.Build.cythonize(['fapswitch/*.py', 'fapswitch/*/*.py', 'fapswitch/*/*/*.py'], exclude=['fapswitch/extensions/fragments.py'])
-    cmdclass.update({ 'build_ext': build_ext })
-
 scripts = glob('bin/*')
 
 setup(name='fapswitch',
-      version='7.1.1',
-      description='Functional group modification on cifs',
+      version='7.1.2',
+      description='Functional group modification on CIF files',
       long_description=open('README.rst').read(),
       author='Tom Daff',
       author_email='tdaff@uottawa.ca',
@@ -36,13 +21,10 @@ setup(name='fapswitch',
                          'web/templates/*html', 'web/css/*.css',
                          'web/datastore/*cif']},
       scripts=scripts,
-      cmdclass=cmdclass,
-      ext_modules=ext_modules,
       classifiers=["Programming Language :: Python",
                    "Programming Language :: Python :: 3",
                    "Development Status :: 5 - Production/Stable",
                    "Intended Audience :: Science/Research",
                    "License :: OSI Approved :: BSD License",
                    "Operating System :: OS Independent",
-                   "Topic :: Scientific/Engineering :: Chemistry",
-                   ])
+                   "Topic :: Scientific/Engineering :: Chemistry"])
